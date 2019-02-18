5a8ae16cafb0cf3ea33f2964ca3e0443db99fdcd
Miguel Angel Alcalá Rios <migue@S1001BAP5W10.softiner.com>
2019-02-18 18:44:24 -0500

    [INFO]-Adición del nuevo componente Tomacorriente y cambio en el menú lateral.

---
+++ front/src/app/tomacorriente/tomacorriente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tomacorriente',
+  templateUrl: './tomacorriente.component.html',
+  styleUrls: ['./tomacorriente.component.css']
+})
+export class TomacorrienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
